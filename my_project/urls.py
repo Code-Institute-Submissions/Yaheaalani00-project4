8f0583c1510afd34f2c4015cd2f433a57de1c9fa
Yaheaalani00 <>
2024-01-30 08:33:25 +0000

    added new url

--- my_project/urls.py
+++ my_project/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from . import views
+from reservations import views
 
 urlpatterns = [
-    path('', views.reservation_list, name='reservation_list'),
-    path('create/', views.create_reservation, name='create_reservation'),
+    # Add paths to reservations app views
+    path('reservations/', views.reservation_list, name='reservation_list'),
+    path('reservations/create/', views.create_reservation, name='create_reservation'),
+    # Other URL patterns...
 ]
