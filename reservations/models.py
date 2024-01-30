43ea7c0d3f4b9ef1d2b4a1f2f59eeea4d18ed36e
Yaheaalani00 <>
2024-01-30 09:01:50 +0000

    a

--- reservations/models.py
+++ reservations/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 
-class Reservation(models.Model):
-    guest_name = models.CharField(max_length=100)
-    reservation_date = models.DateField()
-    reservation_time = models.TimeField()
-    num_guests = models.IntegerField()
 
-    def __str__(self):
-        return f"{self.guest_name} - {self.reservation_date} - {self.reservation_time}"
+class Reservation(models.Model):
+    # Your model fields here
+
+    class Meta:
+        app_label = 'reservations'
